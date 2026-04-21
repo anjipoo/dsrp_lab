6f1f09f04224e445a51fc5105deefa2a67a854b0
anjipoo <anjanarajagopal2004@gmail.com>
2026-04-21 22:31:05 +0530

    commented

--- lab8.R
+++ lab8.R
@@ -2,19 +2,19 @@
 
 data(mtcars)
 df<-mtcars
-summary(df)
-str(df)
+summary(df)  #shows mean, median, min, max 
+str(df)  #shows structure: cols, dtypes, some example values
 head(df)
 
 #processing
-df$am<-as.factor(df$am)
+df$am<-as.factor(df$am)  #making am as factor for classification
 df_clean<-df
 
 #tts
-set.seed(123)
+set.seed(123)  #for reproduceability
 
 library(caret)
-train_idx<-createDataPartition(df_clean$am, p=0.7, list = FALSE)
+train_idx<-createDataPartition(df_clean$am, p=0.7, list = FALSE)  #cDP for class balance
 train<-df_clean[train_idx,]
 test<-df_clean[-train_idx,]
 
@@ -22,28 +22,28 @@ test<-df_clean[-train_idx,]
 
 # install.packages("randomForest")
 library(randomForest)
-model_cls<-randomForest(am~., data=train, ntree=100)
-print(model_cls)
+#takes random samples -> builds multiple trees -> uses random features -> then classif (majority) or regression (avg)
+model_cls<-randomForest(am~., data=train, ntree=100)  #predict am using all features, ntree=no. of trees
+print(model_cls)  #gives error rate, confu matrix, out of bag errors 
+#OOB- It provides an unbiased estimate of the model's prediction error on unseen data without the need for a separate validation set or cross-validation. 
+# oob uses samples that were not included in any bootstrap sampling
 
 #prediction
 pred_cls<-predict(model_cls, test)
-confusionMatrix(pred_cls, test$am)
+confusionMatrix(pred_cls, test$am)  #acc, prec, rec, f1
 
 #vizzzzz
-plot(model_cls)
-varImpPlot(model_cls)
+plot(model_cls)  #shows error vs no. of trees
+varImpPlot(model_cls)  #shows feature importance
+#uses meandecreasegini (Sum of all reductions in Gini Impurity caused by a feature across all splits (and trees), averaged → higher total reduction = higher importance) and meandecreaseaccuracy (Train model → shuffle one feature → measure drop in accuracy → average this drop over all trees → bigger drop = more important feature.)
 
 #feature imp
 library(ggplot2)
 
-imp <- as.data.frame(importance(model_cls))
+imp <- as.data.frame(importance(model_cls))  #cvt importance into df
 imp$Feature <- rownames(imp)
 
-ggplot(imp, aes(reorder(Feature, MeanDecreaseGini), MeanDecreaseGini)) +
-  geom_col(fill = "darkgreen") +
-  coord_flip() +
-  labs(title = "Feature Importance")
-
+ggplot(imp, aes(reorder(Feature, MeanDecreaseGini), MeanDecreaseGini)) + geom_col(fill = "darkgreen") + coord_flip() + labs(title = "Feature Importance")
 
 #regression random forest
 
@@ -64,17 +64,17 @@ test<-df_clean[-train_idx,]
 #model
 
 library(randomForest)
-model_reg<-randomForest(mpg~., data=train, ntree=100)
+model_reg<-randomForest(mpg~., data=train, ntree=100)  #mpg is the target var
 print(model_reg)
 
 #pred
 pred_reg<-predict(model_reg, test)
 
-residuals<-pred_reg-test$mpg
+residuals<-pred_reg-test$mpg  #error = actual-pred
 
-rmse<-sqrt(mean(residuals^2))
+rmse<-sqrt(mean(residuals^2))  #rmse = sqrt of mean of error^2 -> lower=better
 rmse
-r2<-cor(pred_reg, test$mpg)^2
+r2<-cor(pred_reg, test$mpg)^2  #r2 shows how well model explains variance -> 0=bad, 1=perfect
 r2
 
 #vizzzzz
