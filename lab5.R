c683ec85ec9a7d1c0351398b46fee724e3d8f4f8
anjipoo <anjanarajagopal2004@gmail.com>
2026-04-21 23:35:39 +0530

    commented

--- lab5.R
+++ lab5.R
@@ -9,12 +9,12 @@ str(df)
 #exploration
 summary(df)
 colnames(df)
-colSums(is.na(df))
+colSums(is.na(df))  #shows NA counts for each col
 
 #cleaning
-df$mpg[is.na(df$mpg)]<-mean(df$mpg, na.rm=TRUE)  #fill as mean 
-df$cyl<-as.factor(df$cyl)  #to cvt to categorical
-df$gear<-as.factor(df$gear)
+df$mpg[is.na(df$mpg)]<-mean(df$mpg, na.rm=TRUE)  #replace na with mean
+df$cyl<-as.factor(df$cyl)  #cvt to categorical
+df$gear<-as.factor(df$gear)  #Required for regression to treat them as categories
 
 #train test split
 set.seed(123)
@@ -23,26 +23,30 @@ train<-df[train_idx,]
 test<-df[-train_idx,]
 
 #model
-model=lm(mpg~wt+hp+cyl+gear, data=train)
-summary(model)
+model=lm(mpg~wt+hp+cyl+gear, data=train)  #mpg is target, wt hp cyl gear are predictors
+summary(model)  #shows coeff, r2, p values
 
 pred<-predict(model, newdata=test)
-head(data.frame(Actual=test$mpg, Predicted=pred))
+head(data.frame(Actual=test$mpg, Predicted=pred))  #shows confu matrix
 
 #evals
 residuals<-test$mpg-pred
 MAE<-mean(abs(residuals))
-RMSE<-sqrt(mean(residuals^2))
+RMSE<-sqrt(mean(residuals^2))  #Penalizes large errors
 print(MAE)
 print(RMSE)
 
 #visualizations
 plot(test$mpg, pred, main="actual vs predicted", xlab="actual", ylab="predicted", col="blue")
 abline(0,1,col="red")
-hist(residuals, main="residuals dist", col="orange")
-boxplot(df$mpg, main="mpg dist", col="green")
-cormat<-cor(df[sapply(df,is.numeric)])
+# Points near line → good model
+
+hist(residuals, main="residuals dist", col="orange")  #Should be normally distributed
+boxplot(df$mpg, main="mpg dist", col="green")  #dist of mpg
+cormat<-cor(df[sapply(df,is.numeric)])  #corr heatmap
 heatmap(cormat, main="corr heatmap")
 
 #anova
-anova(model)
+anova(model)  #checks usefulness of each feature that is being added
+# Small p-value (< 0.05) → variable is important
+# Large p-value (> 0.05) → variable may not matter much
