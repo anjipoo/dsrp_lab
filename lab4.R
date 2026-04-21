70fd112f054db9c0f798a764ee149ba50556cc34
anjipoo <anjanarajagopal2004@gmail.com>
2026-04-21 23:50:42 +0530

    commented

--- lab4.R
+++ lab4.R
@@ -8,11 +8,11 @@ colnames(data)
 
 #cleaning
 data_clean<-na.omit(data) #remove missing
-data_clean$Churn<-ifelse(data_clean$Churn=="Yes", 1,0)
+data_clean$Churn<-ifelse(data_clean$Churn=="Yes", 1,0)  #make categorical to numeric cuz proba
 write.csv(data_clean, "cleaned_churn.csv", row.names=FALSE)
 
 #choose imp features
-model<-data_clean[, c("Churn", "tenure", "MonthlyCharges", "SeniorCitizen")]
+model<-data_clean[, c("Churn", "tenure", "MonthlyCharges", "SeniorCitizen")]  #choose imp features
 
 #split & model
 set.seed(123)
@@ -20,7 +20,7 @@ train<-sample(1:nrow(model), 0.7*nrow(model))
 train_data<-model[train,]
 test_data<-model[-train,]
 
-logreg<-glm(Churn~., data = train_data, family = binomial)
+logreg<-glm(Churn~., data = train_data, family = binomial)  #binomial imp otherwise itll work like linear reg
 summary(logreg)
 
 #predictions
@@ -38,6 +38,7 @@ library(pROC)
 roc_obj<-roc(test_data$Churn, pred)
 plot(roc_obj, main="ROC Curve")
 auc(roc_obj)
+#a performance metric for binary classification models, representing the probability that a model ranks a random positive example higher than a negative one
 
 #visualizations
 #target dist
