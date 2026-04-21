c9a7b732f7e5b0c46a23b98bf89b1afd1bad0dac
anjipoo <anjanarajagopal2004@gmail.com>
2026-04-21 23:07:29 +0530

    commented

--- lab6.R
+++ lab6.R
@@ -2,18 +2,18 @@
 
 data(ToothGrowth)
 df<-ToothGrowth
-str(df)
-summary(df)
+str(df)  #col, dtypes, example values
+summary(df)  #mean median min max etc
 head(df)
 
 #basic viz
-boxplot(len~supp, data=df, main="tooth length vs suppl")
+boxplot(len~supp, data=df, main="tooth length vs suppl")  #shows dist of len wrt supplement
 
 #preprocessing
-df$supp<-as.factor(df$supp)
-df$dose<-as.factor(df$dose)
+df$supp<-as.factor(df$supp)  #Naive Bayes handles classification → target must be factor
+df$dose<-as.factor(df$dose)  #Even though numeric, here it represents categories (0.5, 1, 2)
 
-clean<-df
+clean<-df  #save as new df
 
 #tts
 set.seed(123)
@@ -25,15 +25,16 @@ test<-clean[-train_idx,]
 #model
 # install.packages("e1071")
 library(e1071)
-model<-naiveBayes(supp~len+dose, data=train)
+model<-naiveBayes(supp~len+dose, data=train)  #Predict supp using len and dose, model learns Probability distributions of features per class
 
 #pred
-pred<-predict(model, test)
+pred<-predict(model, test)  #predicts oj or vc
 
 #eval
-table(Predicted=pred, Actual=test$supp)
-acc<-mean(pred==test$supp)
+conf<-table(Predicted=pred, Actual=test$supp)  #conf matrix
+print(conf)
+acc<-mean(pred==test$supp)  #corrct/total
 acc
 
 #vizzzz
-plot(test$len, col=pred, main="pred classes")
+plot(test$len, col=pred, main="pred classes")  #scatterplot to viz separation
