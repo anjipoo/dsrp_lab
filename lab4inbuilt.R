eb0f4f6062c466bfa557df6f535d014bde3b7d94
anjipoo <anjanarajagopal2004@gmail.com>
2026-04-21 23:47:04 +0530

    commented

--- lab4inbuilt.R
+++ lab4inbuilt.R
@@ -8,9 +8,10 @@ head(df)
 colnames(df)
 
 #target variable
-df$am<-as.factor(df$am)
+df$am<-as.factor(df$am)  #Classification problems require categorical output
+
 #feature selection
-model_features<-df[, c("am", "mpg", "hp", "wt")]
+model_features<-df[, c("am", "mpg", "hp", "wt")]  #only choose imp features to reduce noise
 
 #train test split and model
 
@@ -19,12 +20,13 @@ train_idx<-sample(1:nrow(model_features), 0.7*nrow(model_features))
 train<-model_features[train_idx,]
 test<-model_features[-train_idx,]
 
-model<-glm(am~., data=train, family = binomial)
-summary(model)
+model<-glm(am~., data=train, family = binomial)  #predict am using other chosen features
+#imp to put family=binomial else it'll work as linear reg
+summary(model)  #showa coefficients etc
 
 #pred
-pred<-predict(model, test, type="response")
-pred_cls<-ifelse(pred>=0.5, 1,0)
+pred<-predict(model, test, type="response")  #returns proba
+pred_cls<-ifelse(pred>=0.5, 1,0)  #cvt proba to class
 pred_cls <- factor(pred_cls, levels = c(0,1))
 
 #eval
@@ -32,15 +34,16 @@ acc<-mean(pred_cls==test$am)
 acc
 
 conf<-table(Predicted=pred_cls, Actual=test$am)
-print(conf)
+print(conf)  #tp tn fp fn 
 
 #viz
 library(pROC)
 roc_obj <- roc(test$am, pred)
 plot(roc_obj)
 auc(roc_obj)
+#a performance metric for binary classification models, representing the probability that a model ranks a random positive example higher than a negative one
 
-hist(df$hp, main="Horsepower Distribution", col="purple")
+hist(df$hp, main="Horsepower Distribution", col="purple")  #Check class balance
 barplot(table(df$am), col="skyblue", main="Transmission Type")
 cor_matrix <- cor(df[, sapply(df, is.numeric)])
 heatmap(cor_matrix)
