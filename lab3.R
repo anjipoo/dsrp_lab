6895712542d479bb34d67972867d88c19161987d
anjipoo <anjanarajagopal2004@gmail.com>
2026-04-22 00:10:56 +0530

    commented

--- lab3.R
+++ lab3.R
@@ -10,19 +10,22 @@ inspect(data[1:5])  #first 5 transactions
 
 #item freq dist
 library(arulesViz)
-itemFrequencyPlot(data, topN=10, type="absolute", main="top 10 items", col="orange")
+itemFrequencyPlot(data, topN=10, type="absolute", main="top 10 items", col="orange")  #shows top 10 items from the trans
 
 #apply apriori
-rules<-apriori(data, parameter = list(supp=0.01, conf=0.5))
+rules<-apriori(data, parameter = list(supp=0.01, conf=0.5))  #supp and conf parameters
+#supp -> times occuring/total trans
+#conf -> proba of rhs given lhs -> supp(a,b)/supp(a)
 summary(rules) #summary of rules
-inspect(head(rules,10))
+inspect(head(rules,10))  #show 10 rules
 
 #sorted by confidence
-sorted_rules<-sort(rules, by="confidence", decreasing = TRUE)
+sorted_rules<-sort(rules, by="confidence", decreasing = TRUE)  #show top 10 rules by conf
 inspect(head(sorted_rules,10))
 
 #sorted by lift
-rules_lft<-sort(rules, by="lift", decreasing = TRUE)
+rules_lft<-sort(rules, by="lift", decreasing = TRUE)  #top 10rules by lift
+#lift -> strength of rule -> conf of rule/supp of item
 inspect(head(rules_lft, 10))
 
 plot(rules, method="scatterplot")
@@ -37,6 +40,6 @@ mil_rules<-apriori(data, parameter = list(supp=0.01, conf=0.5), appearance = lis
 inspect(mil_rules)
 
 #remove redundant
-rules_clean<-rules[!is.redundant(rules)]
+rules_clean<-rules[!is.redundant(rules)]  #removes repeated rules
 inspect(head(rules_clean))
 
